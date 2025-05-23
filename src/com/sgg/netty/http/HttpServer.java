40b0e2ebe952582576b0566ce6713bfbcd593ee7
rayl <liuyi86@zto.com>
2025-05-24 07:37:10 +0800

    feat: 支持Http请求

---
+++ src/com/sgg/netty/http/HttpServer.java
@@ -0,0 +1,31 @@
+package com.sgg.netty.http;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+public class HttpServer {
+    public static void main(String[] args) throws Exception {
+        EventLoopGroup bossGroup = new NioEventLoopGroup(1);
+        EventLoopGroup workerGroup = new NioEventLoopGroup(8);
+
+        try {
+            ServerBootstrap serverBootstrap = new ServerBootstrap();
+            serverBootstrap.group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .childHandler(new MyHttpServerChannelInitializer());
+
+            // 这里的端口使用7200, 6668端口浏览器会提示非安全端口
+            ChannelFuture channelFuture = serverBootstrap.bind(7200).sync();
+
+            System.out.println("server is ready");
+
+            channelFuture.channel().closeFuture().sync();
+        } finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
+}
---
+++ src/com/sgg/netty/http/MyHttpServerChannelInitializer.java
@@ -0,0 +1,18 @@
+package com.sgg.netty.http;
+
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.codec.http.HttpServerCodec;
+
+public class MyHttpServerChannelInitializer extends ChannelInitializer<SocketChannel> {
+    @Override
+    protected void initChannel(SocketChannel ch) throws Exception {
+        // 向管道加入处理器
+        ChannelPipeline pipeline = ch.pipeline();
+
+        pipeline.addLast("MyHttpServerCodec", new HttpServerCodec()); // http 编码解码器
+        pipeline.addLast("MyHttpServerHandler", new MyHttpServerHandler());
+
+    }
+}
