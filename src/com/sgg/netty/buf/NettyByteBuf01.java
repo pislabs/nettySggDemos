d2e51350f664cccb0aef3f0883ab5a8338331c42
rayl <liuyi86@zto.com>
2025-05-26 06:47:51 +0800

    feat: 新增ByteBuf处理

---
+++ src/com/sgg/netty/buf/NettyByteBuf01.java
@@ -0,0 +1,31 @@
+package com.sgg.netty.buf;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+
+public class NettyByteBuf01 {
+    public static void main(String[] args) {
+        // 1. 创建一个对象，对象包含一个数组arr, 是一个byte[10]
+        // 2. 在netty的buffer中，不需要使用flip进行反转
+        //    底层维护了raderindex和writeindex
+        // 3. 通过raderindex和writeindex和capacity将buffer分为三个区
+        //    0 - readindex 已读区域
+        //    readindex - writeindex 可读区域
+        //    writeindex - capacity 可写区域
+        ByteBuf buffer = Unpooled.buffer(10);
+
+        for(int i = 0; i< 10; i++) {
+            buffer.writeByte(i);
+        }
+
+        System.out.println("capacity" + buffer.capacity());
+
+        // 输出
+        for(int i = 0; i< buffer.capacity(); i++) {
+            System.out.println(buffer.readByte());
+            System.out.println(buffer.getByte(i));
+        }
+
+        System.out.println("执行完毕");
+    }
+}
