49ec8c0b1a6270ae0a5cdb34f08661818d494cae
rayl <liuyi86@zto.com>
2025-05-29 06:52:36 +0800

    feat: netty编解码处理

---
+++ src/com/sgg/netty/ioboundhandler/MyClientInitializer.java
@@ -0,0 +1,18 @@
+package com.sgg.netty.ioboundhandler;
+
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.socket.SocketChannel;
+
+public class MyClientInitializer extends ChannelInitializer<SocketChannel> {
+    @Override
+    protected void initChannel(SocketChannel ch) throws Exception {
+        ChannelPipeline pipeline = ch.pipeline();
+
+        // 加入一个出站的handler，对数据进行编码
+        pipeline.addLast(new MyLongToByteEncoder());
+
+        // 加入一个自定义的handler，处理业务
+        pipeline.addLast(new MyClientHandler());
+    }
+}
---
+++ src/com/sgg/netty/ioboundhandler/MyServerInitializer.java
@@ -0,0 +1,18 @@
+package com.sgg.netty.ioboundhandler;
+
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.socket.SocketChannel;
+
+public class MyServerInitializer extends ChannelInitializer<SocketChannel> {
+    @Override
+    protected void initChannel(SocketChannel ch) throws Exception {
+        ChannelPipeline pipeline = ch.pipeline();
+
+        // 入站的handler进行解码MyByteToLongDecoder
+        pipeline.addLast(new MyByteToLongDecoder());
+
+        // 自定义handler进行业务处理
+        pipeline.addLast(new MyServerHandler());
+    }
+}
